181377c2bceee064f086dd8f7e9176270ce26ce3
Rahil S <rahilrshk@gmail.com>
2021-11-02 02:51:57 +0530

    Added streams

---
+++ stream/lets-get-lazy/code/3_eager_vs_lazy_java/Sample.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Sample {
+  public static int expensiveComputation(int input) {
+    System.out.println("called expensive computation");
+    return input;
+  }
+  
+  public static void main(String[] args) {
+    int somevalue = 4;
+    
+    System.out.println("Short circuit");
+    if(somevalue > 5 && expensiveComputation(5) > 0)
+      System.out.println("");
+    else
+      System.out.println("");
+      
+    System.out.println("Not so efficient");
+    int temp = expensiveComputation(5);
+    if(somevalue > 5 && temp > 0)
+      System.out.println("");
+    else
+      System.out.println("");
+  }
+}
---
+++ stream/lets-get-lazy/code/8_efficiency/StockFetcher.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class StockFetcher {
+  public static List<StockInfo> fetchStockPrices(List<String> symbols) {
+    List<StockInfo> stocks = new ArrayList<>();
+    for(String symbol : symbols) {
+      stocks.add(StockUtil.getPrice(symbol));
+    }
+    
+    return stocks;
+  }
+  
+  public static Stream<StockInfo> fetchStockPricesLazy(List<String> symbols) {
+    return symbols.stream()
+                  .map(StockUtil::getPrice);
+  }
+}
